bb5f16d6659e6e50ef14cc2e3365f7e008cbc752
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-09-06 17:48:54 -0700

    test: npm script test:all now works once again
    All the tests are passing as expected.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- pkg/core/src/test/typescript/integration/lint-git-repo-service.test.ts
+++ pkg/core/src/test/typescript/integration/lint-git-repo-service.test.ts
@@ -2,19 +2,18 @@ import path from "path";
 
 import test, { Test } from "tape-promise/tape";
 import temp from "temp";
-import fs from "fs-extra"
+import fs from "fs-extra";
 import { v4 as uuidv4 } from "uuid";
 
 import { LintGitRepoService } from "../../../main/typescript";
 
-import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import simpleGit, { SimpleGit, SimpleGitOptions } from "simple-git";
 
 import { LogLevelDesc } from "@dci-lint/common";
 
 const logLevel: LogLevelDesc = "INFO";
 
 test("finds target word in git repo", async (t: Test) => {
-
   const label = "./dci-lint-core-test-lint-git-repo-service/";
   const targetPhrasePatterns = [uuidv4()];
   const commitMessage = uuidv4();
@@ -43,13 +42,16 @@ test("finds target word in git repo", async (t: Test) => {
 
   const options: SimpleGitOptions = {
     baseDir: testDir,
-    binary: 'git',
+    binary: "git",
     maxConcurrentProcesses: 1,
   };
 
   // Create a dummy git repository locally that we'll clone in the lint git repo svc
   const git: SimpleGit = simpleGit(options);
   await git.init([]);
+  await git.addConfig("user.name", "DCI Lint Test User");
+  await git.addConfig("user.email", "<dci-lint-test-user@example.com>");
+  await git.addConfig("commit.gpgSign", "false");
   await git.add([okFilePath, badFilePath]);
   await git.commit(commitMessage);
 
