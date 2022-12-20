f2abf8ed8f3730516d064cea6dd80783603caaa2
Peter Somogyvari <peter.metz@unarin.com>
2022-12-20 09:58:59 -0800

    fix(test-api-client): attempt #2 on IPv6 NodeJS v18 problems
    Moved from "localhost" to "127.0.0.1" which is explicitly IPv4 so that
    there is no confusion about defaults having been changed.
    
    Signed-off-by: Peter Somogyvari <peter.metz@unarin.com>

--- pkg/test-api-client/src/test/typescript/integration/lint-git-repo-v1-endpoint.test.ts
+++ pkg/test-api-client/src/test/typescript/integration/lint-git-repo-v1-endpoint.test.ts
@@ -22,7 +22,7 @@ test.skip("Lint a git repo", async (t: Test) => {
   webApp.use(bodyParser.json({ limit: "25mb" }));
   const server = http.createServer(webApp);
   const listenOptions: IListenOptions = {
-    hostname: "localhost",
+    hostname: "127.0.0.1",
     port: 0,
     server,
   };
@@ -79,7 +79,7 @@ test("respects the .dcilintignore configuration", async (t: Test) => {
   webApp.use(bodyParser.json({ limit: "25mb" }));
   const server = http.createServer(webApp);
   const listenOptions: IListenOptions = {
-    hostname: "localhost",
+    hostname: "127.0.0.1",
     port: 0,
     server,
   };
@@ -95,11 +95,11 @@ test("respects the .dcilintignore configuration", async (t: Test) => {
   registerWebServiceEndpoint(webApp, endpoint);
 
   const { port } = addressInfo;
-  // started hardcoding this as localhost since NodeJS v18 defaults to IPv6
+  // started hardcoding this as 127.0.0.1 since NodeJS v18 defaults to IPv6
   // which is broken on GitHub CI action workflows for some reason...
   // meaning that the address comes back as "::1" (which equals 127.0.0.1)
   // but then the API server rejects it with a 404
-  const apiHost = `http://localhost:${port}`;
+  const apiHost = `http://127.0.0.1:${port}`;
   LOG.debug("API Host: %o", apiHost);
 
   const configuration = new Configuration({ basePath: apiHost });
